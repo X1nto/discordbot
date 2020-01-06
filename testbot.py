2c0d19a1ce2ad18e8abd55d4448a4619c9931aeb
X1nto <burtkni@yandex.ru>
2020-01-06 15:59:39 +0400

    updated files

--- testbot.py
+++ testbot.py
@@ -27,6 +27,7 @@ async def on_message(message):
        await message.channel.send('Here ya go', file=discord.File('owl.jpg'))
     
     if message.content.startswith('!random'):
+       #this is my image list that's located in this folder, you can use your own image names
        list = '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg'
        await message.channel.send('take this', file=discord.File(random.choice(list)))
 
@@ -36,5 +37,10 @@ async def on_message(message):
     if message.content.startswith('!დღის ბოზი'):
        user = random.choice(message.channel.guild.members)
        await message.channel.send("დღის ბოზი არის %s " % user.mention)
+
+    if message.content.startswith('!about'):
+       #replace <@423915768191647755> with your ID
+       creator = '<@423915768191647755>'
+       await message.channel.send('This bot was created by %s ' % creator) 
 
 client.run(TOKEN)
